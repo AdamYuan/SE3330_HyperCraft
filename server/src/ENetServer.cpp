a2d920524d8159af3b52f85bf73252989a0dea97
AdamYuan <y13916619121@126.com>
2023-05-09 21:02:28 +0800

    Fix compile error

--- server/src/ENetServer.cpp
+++ server/src/ENetServer.cpp
@@ -5,7 +5,7 @@
 
 namespace server {
 
-std::shared_ptr<ENetServer> ENetServer::Create(const std::shared_ptr<WorldDatabase> &level_db, uint16_t port) {
+std::shared_ptr<ENetServer> ENetServer::Create(const std::shared_ptr<common::WorldDatabase> &level_db, uint16_t port) {
 	ENetAddress address;
 	address.host = ENET_HOST_ANY;
 	address.port = port;
